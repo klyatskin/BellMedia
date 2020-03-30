f64106fa38d6554fb5223d46d3d7e647dbcb0988
Konstantin Klyatskin <klyatskin@gmail.com>
2020-03-30 18:44:09 -0400

    Initial Commit

---
+++ BellMedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BellMedia
+//
+//  Created by Konstantin Klyatskin on 2020-03-30.
+//  Copyright © 2020 Konstantin Klyatskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
